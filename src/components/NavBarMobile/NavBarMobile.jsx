eecd3603b4b1fcee2042b847be9b7b9379011d55
Matthias.Urgu <matthias.urgu@gmail.com>
2023-06-28 10:51:52 +0200

    useEffect menu

--- src/components/NavBarMobile/NavBarMobile.jsx
+++ src/components/NavBarMobile/NavBarMobile.jsx
@@ -6,7 +6,7 @@ import Reminders from "../../assets/img/icon-reminders.svg"
 import TodoList from "../../assets/img/icon-todo.svg"
 import Calendar from "../../assets/img/icon-calendar.svg"
 
-import { useState } from "react";
+import { useState, useRef ,useEffect} from "react";
 import {motion} from 'framer-motion'
 import s from "./style.module.scss";
 
@@ -15,6 +15,10 @@ function NavBarMobile() {
   const [isFeatures, setFeatures] = useState(false);
   const [isCompagny, setCompagny] = useState(false);
 
+  let menuRef = useRef()
+
+
+  /* State pour les menus */
   function hamburgerMenu() {
     setHamburger(!isHamburger);
   }
@@ -42,18 +46,35 @@ const CompagnyUp = <span className={s.chevron}><ChevronDoubleUp size={10}/></spa
 /* const switch Compagny Down */
 const CompagnyDown = <span className={s.chevron}><ChevronDoubleDown size={10}/></span>
 
-const animateFrom = {opacity:0 , y:-10}
+
+
+
+/* framer motion animation */
+const animateFrom = {opacity:0 , y:-20}
 const animateTo = {opacity:1 , y:0 }
 
+/* useEffect pour écouter et désactiver les menus  */
+useEffect(()=>{
+  let cancel =(e)=>
+    {
+      if (!menuRef.current.contains(e.target)){
+        setHamburger(false)
+        
+      }
+    }
+    document.addEventListener('mousedown', cancel)
+})
+
+
   return (
-    <div className={s.header_mobile}>
+    <div className={s.header_mobile} ref={menuRef}>
       {isHamburger ? hamburgerOn : hamburgerOff}
       <div className={s.hamburgerShow} style={{ display: isHamburger ? "block" : "none" }}>
         <ul className={s.hamburgerList}>
           <motion.li
             initial={animateFrom}
             animate={isHamburger ?animateTo : animateFrom}
-            transition={{delay:3}} className={s.featuresLi} onClick={featuresMenu}><a href="#">Features {isFeatures ? featuresUp : featuresDown}</a></motion.li>
+            transition={{delay:0.6}} className={s.featuresLi} onClick={featuresMenu}><a href="#">Features {isFeatures ? featuresUp : featuresDown}</a></motion.li>
             <ul className={s.featuresMenu} style={{display: isFeatures ? "flex" : "none"}} >
                 <li><a href="#"><img src={TodoList} alt="TodoList"/> To do list</a></li>
                 <li><a href="#"><img src={Calendar} alt="Calendar"/> Calendar</a></li>
@@ -63,7 +84,7 @@ const animateTo = {opacity:1 , y:0 }
           <motion.li
             initial={animateFrom}
             animate={isHamburger ?animateTo : animateFrom}
-            transition={{delay:3}} onClick={compagnyMenu}><a href="#">Compagny {isCompagny ? CompagnyUp : CompagnyDown} </a></motion.li>
+            transition={{delay:0.7}} onClick={compagnyMenu}><a href="#">Compagny {isCompagny ? CompagnyUp : CompagnyDown} </a></motion.li>
           <ul className={s.compagnyMenu} style={{display: isCompagny ? "flex" : "none"}} >
                 <li><a href="#">History</a></li>
                 <li><a href="#">Our Team</a></li>
@@ -72,11 +93,11 @@ const animateTo = {opacity:1 , y:0 }
           <motion.li
             initial={animateFrom}
             animate={isHamburger ? animateTo : animateFrom}
-            transition={{delay:3}}><a href="#">Careers</a></motion.li>
+            transition={{delay:0.8}}><a href="#">Careers</a></motion.li>
           <motion.li
             initial={animateFrom}
             animate={isHamburger ?animateTo : animateFrom}
-            transition={{delay:3}}><a href="#">About</a></motion.li>
+            transition={{delay:0.9}}><a href="#">About</a></motion.li>
         </ul>
         <div className={s.hamburgerButton} >
             
